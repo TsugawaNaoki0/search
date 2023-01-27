17ca339de5f586030f9f5b6b02e1507d81b6aa43
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2023-01-27 20:12:29 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <form class="" action="./result.php" method="post">
+      <div class="">
+        <input type="text" name="text" value="">
+        <input type="submit" name="" value="検索">
+      </div>
+    </form>
+  </body>
+</html>
